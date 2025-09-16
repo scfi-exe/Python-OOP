02bfbe86f547fcc68ed4393a1193c8bed57b1b25
scfi-exe <scottfinkelstein97@gmail.com>
2025-09-16 17:55:11 -0400

    Abstract Method and Class

--- 6. Abstraction/02. Abstract Method and Class.py
+++ 6. Abstraction/02. Abstract Method and Class.py
@@ -57,3 +57,59 @@ class Superman(Superhero):
 #          It should take the [amt] as an argument and subtract it from the balance.
 #          If the balance is less than the amount, it should return "Insufficient funds"
 #          Otherwise, it should return "Payment successful" and update the balance
+
+
+from abc import ABC, abstractmethod
+
+
+class PaymentCard:
+    def __init__(self, card_number: str, balance: float):
+        self.card_number = card_number
+        self.balance = balance
+
+    # TODO: Implement the process_payment method
+    @abstractmethod
+    def process_payment(amt: float) -> str:
+        pass
+
+
+# TODO: Implement the DebitCard class
+class DebitCard(PaymentCard):
+    def __init__(self, card_number: str, balance: float):
+        super().__init__(card_number, balance)
+
+    def process_payment(self, amt: float) -> str:
+        if self.balance < amt:
+            return "Insufficient funds"
+        else:
+            self.balance -= amt
+            return "Payment successful"
+
+
+# TODO: Implement the CreditCard class
+
+
+class CreditCard(PaymentCard):
+    def __init__(self, card_number: str, balance: float):
+        super().__init__(card_number, balance)
+
+    def process_payment(self, amt: float) -> str:
+        self.balance -= amt
+        return "Payment successful"
+
+
+# Don't modify the code below
+debit_card = DebitCard("1234", 100.0)  # Card with $100 balance
+credit_card = CreditCard("5678", 100.0)  # Card with $100 balance
+
+# Test debit card
+print(debit_card.process_payment(50.0))
+print(debit_card.balance)
+print(debit_card.process_payment(100.0))
+print(debit_card.balance)
+
+# Test credit card
+print(credit_card.process_payment(50.0))
+print(credit_card.balance)
+print(credit_card.process_payment(100.0))
+print(credit_card.balance)
